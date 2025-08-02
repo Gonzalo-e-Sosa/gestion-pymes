07cb783ea85cf354d7dd273c488e0c6fe6ab6b6a
Gonza <gonzalososa342@gmail.com>
2025-08-02 00:17:03 -0300

    Plugins for astro and i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+import solidJs from '@astrojs/solid-js';
+import db from '@astrojs/db';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [solidJs({ devtools: true }), db()],
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es',
+  },
+});
